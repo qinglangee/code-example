fa7e7fddbf98c9323dbd668229168e7e1b6923f1
zhch <zhchqingdao@163.com>
2012-07-13 16:19:19 +0800

    asm first

---
+++ src/main/java/com/zhch/asm/account/Account.java
@@ -0,0 +1,15 @@
+package com.zhch.asm.account;
+
+/**
+ * 一个账号类,被用来改造的
+ * @author lifeix
+ *
+ */
+public class Account {
+	
+	double money = 33;
+	
+	public void showMoney(){
+		System.out.println("my money is :" + money);
+	}
+}
---
+++ src/main/java/com/zhch/asm/account/CheckAccount.java
@@ -0,0 +1,11 @@
+package com.zhch.asm.account;
+
+public class CheckAccount {
+
+	/**
+	 * 在Account.showMoney() 之前进行check
+	 */
+	static public void checkAccount(){
+		System.out.println("check Account.");
+	}
+}
