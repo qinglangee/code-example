9e6e2a0270401ce06b1e02eee213a2bc60266d87
chengz <chengz@staff.l99.com>
2015-05-19 10:16:47 +0800

    add uuid example

---
+++ src/main/java/com/zhch/example/java/UUIDDemo.java
@@ -0,0 +1,10 @@
+package com.zhch.example.java;
+
+import java.util.UUID;
+
+public class UUIDDemo {
+
+	public static void main(String[] args) {
+		System.out.println(UUID.randomUUID().toString());
+	}
+}
